4fe8f8e19ec533d7694c86fa7d02fb2b76261d7a
Francisco Figueroa <91153878+Frankcisco99@users.noreply.github.com>
2023-07-19 15:27:28 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,11 +1,10 @@
-import turtle 
-
+import turtle
+#Pantalla
 pantalla = turtle.Screen()
-pantalla.title("Pong Game")
 pantalla.bgcolor("black")
+pantalla.title("Pong Game")
 pantalla.setup(600,400)
-
-#paleta izquierda
+#Paleta izquierda
 paleta_izq = turtle.Turtle()
 paleta_izq.shape("square")
 paleta_izq.color("white")
@@ -13,7 +12,7 @@ paleta_izq.shapesize(stretch_len=1,stretch_wid=5)
 paleta_izq.penup()
 paleta_izq.goto(-250,0)
 
-#paleta derecha
+#Paleta derecha
 paleta_der = turtle.Turtle()
 paleta_der.shape("square")
 paleta_der.color("white")
@@ -21,18 +20,19 @@ paleta_der.shapesize(stretch_len=1,stretch_wid=5)
 paleta_der.penup()
 paleta_der.goto(250,0)
 
-balon = turtle.Turtle()
-balon.shape("circle")
-balon.color("#C1ECE4")
-balon.penup()
-balon.dx = 5
-balon.dy = -5
+pelota = turtle.Turtle()
+pelota.shape("circle")
+pelota.color("white")
+pelota.penup()
+pelota.dx = 5
+pelota.dy = -5
+
 def paleta_izq_up():
     y = paleta_izq.ycor()
     y = y + 20
     paleta_izq.sety(y)
 
-def paleta_izq_down():
+def paleta_izq_dwn():
     y = paleta_izq.ycor()
     y = y - 20
     paleta_izq.sety(y)
@@ -42,41 +42,63 @@ def paleta_der_up():
     y = y + 20
     paleta_der.sety(y)
 
-def paleta_der_down():
+def paleta_der_dwn():
     y = paleta_der.ycor()
     y = y - 20
     paleta_der.sety(y)
 
 pantalla.listen()
 pantalla.onkeypress(paleta_izq_up,"w")
-pantalla.onkeypress(paleta_izq_down,"s")
+pantalla.onkeypress(paleta_izq_dwn,"s")
 pantalla.onkeypress(paleta_der_up,"Up")
-pantalla.onkeypress(paleta_der_down,"Down")
+pantalla.onkeypress(paleta_der_dwn,"Down")
 
+puntaje_izq = 0
+puntaje_der = 0
+
+tablero = turtle.Turtle()
+tablero.color("white")
+tablero.goto(0,150)
+tablero.write(f"Player1 {puntaje_izq} - Player2 {puntaje_der}",align="center", font=("Verdana",24,"normal"))
 while True:
     pantalla.update()
 
-    balon.setx(balon.xcor() + balon.dx)
-    balon.sety(balon.ycor() + balon.dy)
+    pelota.sety(pelota.ycor() + pelota.dy)
+    pelota.setx(pelota.xcor() + pelota.dx)
 
-    if balon.ycor() > 144:
-        balon.sety(144)
-        balon.dy = balon.dy * -1
-    if balon.ycor() < -144:
-        balon.sety(-144)
-        balon.dy = balon.dy * -1
-    if balon.xcor() > 265:
-        balon.goto(0,0)
-    if balon.xcor() < -265:
-        balon.goto(0,0)    
+    if pelota.ycor() > 140:
+        pelota.sety(140)
+        pelota.dy = pelota.dy * -1
+    
+    if pelota.ycor() < -140:
+        pelota.sety(-140)
+        pelota.dy = pelota.dy * -1
 
-    if paleta_izq.ycor() >= 145:
+    if pelota.xcor() > 250:
+        pelota.goto(0,0)
+        pelota.dx = pelota.dx * -1
+        puntaje_izq = puntaje_izq + 1
+    if pelota.xcor() < -250:
+        pelota.goto(0,0)
+        pelota.dx = pelota.dx * -1
+        puntaje_der = puntaje_der + 1
+    if (pelota.xcor() > 230 and pelota.xcor() < 240) and (pelota.ycor() < paleta_der.ycor() + 40 and pelota.ycor() > paleta_der.ycor()- 40):
+        pelota.setx(230)
+        pelota.dx = pelota.dx * -1
+
+    if (pelota.xcor() < -230 and pelota.xcor() > -240) and (pelota.ycor() < paleta_izq.ycor() + 40 and pelota.ycor() > paleta_izq.ycor()- 40):
+        pelota.setx(-230)
+        pelota.dx = pelota.dx * -1
+    if paleta_izq.ycor() > 145:
         paleta_izq.sety(145)
-    if paleta_izq.ycor() <= -145:
+
+    if paleta_izq.ycor() < -145:
         paleta_izq.sety(-145)
-    if paleta_der.ycor() >= 145:
+    
+    if paleta_der.ycor() > 145:
         paleta_der.sety(145)
-    if paleta_der.ycor() <= -145:
+
+    if paleta_der.ycor() < -145:
         paleta_der.sety(-145)
 
 turtle.done()
